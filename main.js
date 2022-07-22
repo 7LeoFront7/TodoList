045cb18af12264a78b5f6e15aaded7dafbe98d61
Максим <98467035+7LeoFront7@users.noreply.github.com>
2022-07-22 17:13:18 +0300

    New todo + Diz

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 let todos = [];
 let todoNode = document.querySelector('.js-todo')
-let inputNode = document.querySelector('.js-input')
 let btnNode = document.querySelector('.js-btn')
+let inputNode = document.querySelector('.js-input')
 
+btnNode.addEventListener('click', () =>{
+    
+    let text = inputNode.value;
+    inputNode.value ='';
+
+    addTodo(text)
+    render()
+})
+
+todoNode.addEventListener('click', (event) => {
+
+    if (event.target.tagName !== 'BUTTON'){
+        return
+    }
+
+    let id = event.target.dataset.id;
+
+    deleteTodo(id)
+    render()
+})
 
 function addTodo(text){
-
+    
     const todo = {
         text,
         done:false,
@@ -15,48 +35,32 @@ function addTodo(text){
     todos.push(todo)
 }
 
-btnNode.addEventListener('click', function(){
-    addTodo(inputNode.value);
-    inputNode.value = '';
-    render()
-})
-
-todoNode.addEventListener('click', (event) => {
-    if (event.target.tagName !== 'BUTTON'){
-        return
-    }
-
-    let id = event.target.dataset.id;
-
-    deleteTodo(id);
-    render()
-})
-
-
-
-
 function deleteTodo(id){
-
-todos.forEach(todo => {
-    if (todo.id === id){
-        todo.done = true;
-    }
-})
+    
+    todos.forEach(todo => {
+        if(todo.id === id){
+            todo.done = true;
+        }
+    })
 }
 
 function render(){
-    html = '';
+
+    
+
+    let html = '';
 
     todos.forEach(todo =>{
-        if (todo.done == true){
-            return
-        }
 
-        
-        html += `<div>${todo.text}
-                        <button data-id="${todo.id}">Сделано</button>
-                        </div>`
+    if(todo.done){
+        return
+    }    
 
+    html += `<div class="new-todo">${todo.text}
+    <button class="new-btn-com" data-id="${todo.id}">Сделано</button>
+    </div>`
     })
+
     todoNode.innerHTML = html
 }
+
